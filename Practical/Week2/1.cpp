a290b4d7c726052baf80d87b6bcb8e434d2355e0
AnushkaDeoli2706 <77988799+AnushkaDeoli2706@users.noreply.github.com>
2022-03-07 20:02:07 +0530

    Update 1.cpp

--- Practical/Week2/1.cpp
+++ Practical/Week2/1.cpp
@@ -1,3 +1,3 @@
-//
+//Given a sorted array of integers containing few duplicate elements , design an algo and implement it using program to find whether the given key is present in array or not . If present find number of copies of a given key .
 
 
