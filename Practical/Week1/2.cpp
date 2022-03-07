121557871371077ce3f600625b50def0a42189b7
AnushkaDeoli2706 <77988799+AnushkaDeoli2706@users.noreply.github.com>
2022-03-07 10:26:19 +0530

    Update 2.cpp

--- Practical/Week1/2.cpp
+++ Practical/Week1/2.cpp
@@ -11,3 +11,22 @@ int lsearch(int *arr,int n,int key)
     }
     return -1;
 }
+int main()
+{
+    int arr[50];
+    int n;
+    cout<<"Enter the number of elements in array"<<endl;
+    cin>>n;
+    int key;
+    cout<<"enter key value"<<endl;
+    cin>>key;
+    cout<<"enter values"<<endl;
+    for(int i=0;i<n;i++)
+    {     cin>>arr[i];
+    }
+    int r=lsearch(arr,n,key);
+    if(r==-1)
+        cout<<"key not found"<<endl;
+    else
+        cout<<"key found at index:"<<r<<endl;
+}
